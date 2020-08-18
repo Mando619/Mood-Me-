dd567bbeb6207f24957867f2e9b9f5733378ab52
Armando Estrada <libryum33@hotmail.com>
2020-08-17 17:08:01 -0700

    worked on the footer and mobile response.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Mood-Me-
+## Mood-Me
 repository to work on for project
+
+## Description
+
+## Technology Used
+
+## Copyright
+
+
+
+
+[Mood-Me]( https://mando619.github.io/Weather-Dashboard/).
+
+![2020-08-17 (2)](https://user-images.githubusercontent.com/65925169/90453830-83e16080-e0a6-11ea-89ee-16b975814218.png)
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,49 @@
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
+
 $("#search").on("click", function(){
     console.log("ola");
+});
+
+
+
+
+
+
+var settings = {
+  "url": "https://api.spotify.com/v1/playlists/37i9dQZF1DX7qK8ma5wgG1?si=NlA7VeAXTTmcTS0oCT8cqw",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Bearer BQD-outpOdnWriG4EPdd60Xa001m2AGU5LvLocZQGkRedd76c6G8_cj2kUDMe6DCg29EH3JS4kvoEK_UaPMOM_YTd4ULJ193brhP0HlZwuZfoQQBR-Gbikf6NuCVIKrJDeszD126ad4v9xFxVzpYd-6_0YYhqtNVcS04VZ2Mj52aMNKeKzVMYhSi90LOt0IQ2fe2xkyXZnfSZC16h34Nl6ahhggKo77e8zK5xvLsjjQHaQI8mrOZDpOcOsJ5pQnm71KMijPyvtLDtXzp"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response.external_urls.spotify);
+  let url = response.external_urls.spotify;
+
+  let embedLink = url.replace('https://open.spotify.com/playlist/', 'https://open.spotify.com/embed/playlist/')
+  console.log(embedLink)
+
+  $("#sad").on("click", function(){
+    console.log(response.external_urls.spotify)
+    $("#playlist").attr("src", embedLink);
+  })
 });
